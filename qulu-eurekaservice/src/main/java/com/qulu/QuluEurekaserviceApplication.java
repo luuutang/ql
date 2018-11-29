28850031e1aff1b35699f6b932251d95c77b3ab2
luuu <769038407@qq.com>
2018-11-29 15:30:27 +0800

    1111

--- qulu-eurekaservice/src/main/java/com/qulu/QuluEurekaserviceApplication.java
+++ qulu-eurekaservice/src/main/java/com/qulu/QuluEurekaserviceApplication.java
@@ -2,11 +2,13 @@ package com.qulu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class MicroserviceApplication {
+@EnableEurekaServer
+public class QuluEurekaserviceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MicroserviceApplication.class, args);
+        SpringApplication.run(QuluEurekaserviceApplication.class, args);
     }
 }
--- qulu-eurekaservice/src/test/java/com/qulu/QuluEurekaserviceApplicationTests.java
+++ qulu-eurekaservice/src/test/java/com/qulu/QuluEurekaserviceApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MicroserviceApplicationTests {
+public class QuluEurekaserviceApplicationTests {
 
     @Test
     public void contextLoads() {
