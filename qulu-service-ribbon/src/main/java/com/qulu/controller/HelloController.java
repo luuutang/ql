1a320ca0147387ee603b29347bf6b4637a67b1e3
luuu <769038407@qq.com>
2018-11-29 17:41:23 +0800

    最新代码

---
+++ qulu-service-ribbon/src/main/java/com/qulu/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.qulu.controller;
+
+import com.qulu.testService.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @Autowired
+    HelloService helloService;
+
+    @GetMapping(value = "/hi")
+    public String hi(@RequestParam String name) {
+        return helloService.hiService( name );
+    }
+}
